cfef4412e1cb3809bfa6b468f5ffe6f737140a5c
TruongTamAn2004 <truongtaman2004@gmail.com>
2023-04-28 13:00:57 +0700

    Update: My Own Testing Scence and Character Script
    Upgrade: Dash method

--- Project 3 GameDevClub -  Top down game/Assets/Script/Character.cs
+++ Project 3 GameDevClub -  Top down game/Assets/Script/Character.cs
@@ -17,6 +17,8 @@ public class Character : MonoBehaviour {
     [SerializeField] protected float _maxWeight;
     [SerializeField] protected float _maxMana;
     [SerializeField] protected float _dashForce;
+    [SerializeField] protected float _minDamageWeight;
+    [SerializeField] protected float _maxDamggeWeight;
 
     [Header("Cost Info")]
     [SerializeField] protected float _dashManaCost;
@@ -24,13 +26,12 @@ public class Character : MonoBehaviour {
     [SerializeField] protected float _skill2ManaCost;
 
     [Header("Debug Info")]
-    [SerializeField] private float _curMoveSpeed;
     [SerializeField] private float _curWeight;
     [SerializeField] private float _curMana;
 
     //Processing Variables
-    private bool _dashAvailable;
-    private float _dashDuration = 2f;
+    private bool _isDashing = false, _isDashAvailable = true;
+    private float _dashLength = 0.1f, _dashCoolDown = 5f;
     private Rigidbody2D _rb;
     private Vector2 _moveDir;
 
@@ -43,6 +44,13 @@ public class Character : MonoBehaviour {
         float Y = Input.GetAxisRaw("Vertical");
         _moveDir.x = X != 0 ? X : _moveDir.x;
         _moveDir.y = Y != 0 ? Y : _moveDir.y;
+        if (Input.GetKeyDown(KeyCode.Space) && _isDashAvailable) StartCoroutine(Dash()); 
+    }
+
+    public virtual void FixedUpdate() {
+        if (!_isDashing) 
+            _rb.velocity = _moveDir * (_minMoveSpeed + _moveSpeedScale / _curWeight);
+        _moveDir = new Vector2(0,0);
     }
 
     public virtual void Attack() {
@@ -50,8 +58,16 @@ public class Character : MonoBehaviour {
     }
 
     public virtual IEnumerator Dash() {
-        _dashAvailable = false;
-        _rb.AddForce(_moveDir * _dashForce, ForceMode2D.Impulse);
-        yield return WaitForSeconds(_dashDuration);
+        _isDashing = true;
+        _isDashAvailable = false;
+        _rb.velocity = _moveDir * _dashForce;
+        yield return new WaitForSeconds(_dashLength);
+        _isDashing = false;
+        _rb.velocity = new Vector2(0,0);
+        yield return new WaitForSeconds(_dashCoolDown);
+        _isDashAvailable = true;
     }
+
+    public abstract void Skill1();
+    public abstract void Skill2();
 }
